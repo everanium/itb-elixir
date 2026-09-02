9f38fac99b4a9af2d1344709f1fbbca0529bb232
Andrey Kuvshinov <andrew@encloud.blue>
2026-09-03 06:26:23 +0700

    sync from monorepo 59634d9
    Source: github.com/everanium/itb @ 59634d9b407e237110e151197664f03086fa4a10

--- lib/itb.ex
+++ lib/itb.ex
@@ -294,7 +294,7 @@ defmodule ITB do
   # Runtime + diagnostics
   # ------------------------------------------------------------------
 
-  @doc ~S(The libitb library version string, e.g. `"0.3.3"`.)
+  @doc ~S(The libitb library version string, e.g. `"0.3.4"`.)
   @spec version() :: {:ok, binary()} | {:error, reason()}
   def version, do: :itb.version()
 
